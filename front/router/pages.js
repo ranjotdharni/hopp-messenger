d9ef7be1943a4297bf4e646a9b188e988ff7a1cc
Robby <113494281+ranjotdharni@users.noreply.github.com>
2022-11-02 02:55:16 -0700

    Update pages.js

--- front/router/pages.js
+++ front/router/pages.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/portal', (req, res) =>
+router.get('/', (req, res) =>
 {
     res.sendFile(path.resolve(__dirname + '/../index.html'));
 });
